a99b1c202ae134e9dde9db472093f3fa4a6f77a0
glocky1 <97502148+glocky1@users.noreply.github.com>
2022-12-23 17:23:21 +0900

    Delete entities.lua

--- entities.lua
+++
@@ -1,5 +0,0 @@
-return {
-    AllEntities={"all", "ambush", "eyes", "glitch", "grundge", "halt", "hide", "none", "random", "rush", "screech", "seek", "shadow", "smiler", "timothy", "trashbag", "trollface"},
-    DeveloperEntities={"trollface"},
-    CustomEntities={"grundge","smiler", "trashbag"}
-}
